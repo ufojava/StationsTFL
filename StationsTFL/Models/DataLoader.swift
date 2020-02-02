dd974a4df00002d210d90c35354ec496b6f0b9a2
Ufuoma Okoro <ufookoro@gmail.com>
2020-02-02 23:46:01 +0000

    Initial commit

---
+++ StationsTFL/Models/DataLoader.swift
@@ -0,0 +1,89 @@
+//
+//  DataLoader.swift
+//  StationsTFL
+//
+//  Created by Ufuoma Okoro on 02/02/2020.
+//  Copyright © 2020 Ufuoma Okoro. All rights reserved.
+//
+
+import Foundation
+
+/*
+ This class is responsible to decoding JSON file
+ It will utilise the data structure - StationDataStructure
+ */
+public class DataLoader: ObservableObject {
+    
+    @Published var dataStructure = [Stations]()
+    
+    //Insert initilize load Data
+    
+    init() {
+        
+        //Decode File
+        decodeJSONFile()
+        
+        //Sorted Data
+        sortData()
+        
+        
+    }
+    
+
+
+//Function to load file
+
+func decodeJSONFile() {
+    
+    //File URL using if let to prevent crashes
+    if let fileLocation = Bundle.main.url(forResource: "TFLStations", withExtension: "json") {
+        
+        //Do try catch
+        
+        do {
+            
+            //Define Data
+            let data = try Data(contentsOf: fileLocation)
+            
+            //Define Decoder
+            let jsonDecode = JSONDecoder()
+            
+            //Decode the JSON Data
+            let dataFromJSON = try jsonDecode.decode([Stations].self, from: data)
+            
+            
+            //Allow for the data to load
+            DispatchQueue.main.async {
+                self.dataStructure = dataFromJSON
+                
+            }//Async
+            
+            
+            
+        } catch { //File Location
+            
+            print(error)
+            
+        }//Catch closure
+        
+        
+    }//If let File search End
+    
+    
+    
+    }//Function decoder closure
+    
+    
+    //Function to sort data
+    func sortData() {
+        
+        //Sort Data Station lines
+        self.dataStructure = self.dataStructure.sorted(by: {$0.lines == $1.lines})
+        
+        
+    }
+    
+    
+    
+    
+}//Class Closure
---
+++ StationsTFL/Models/StationDataStructure.swift
@@ -0,0 +1,23 @@
+//
+//  StationDataStructure.swift
+//  StationsTFL
+//
+//  Created by Ufuoma Okoro on 02/02/2020.
+//  Copyright © 2020 Ufuoma Okoro. All rights reserved.
+//
+
+//Data Structure for Stations
+import SwiftUI
+
+struct Stations: Codable, Identifiable {
+    
+    public var id: Int
+    
+    public var tla: String
+    public var name:    String
+    public var lines:   String
+    
+    
+}
+
+
